ec8ecec2a063153f8b70394563889f3429d88025
rajababu3 <rbtheroot@gmail.com>
2016-09-14 12:38:41 +0530

    added question as comment

--- MITPython/Week1P2.py
+++ MITPython/Week1P2.py
@@ -4,6 +4,11 @@ Created on Wed Sep 14 12:14:23 2016
 
 @author: Raja
 """
+#==============================================================================
+# Write a program that prints the number of times the string 'bob' occurs in s.
+#  For example, if s = 'azcbobobegghakl', then your program should print
+#  Number of times bob occurs is: 2
+#==============================================================================
 
 s = input("Enter a string: ")
 bobCount = 0
